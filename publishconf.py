1c493ce8603541ae23ac3f6b988953b80cfbfb8d
Flávia <flavithalopes@hotmail.com>
2020-09-14 06:31:38 -0300

    Renomeação
    Renomeia includes retirando o prefixo 'section', renomeia blocos em index.html, retirando prefixo 'section' e reorganiza alguns arquivos.

--- publishconf.py
+++ publishconf.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 import os
 import sys
+
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 sys.path.append(os.curdir)
